e6493f8469ee11bdb5f69b61ac87c79ceb313bfc
Victor14791012 <victor.carvalhok23@gmail.com>
2023-10-19 16:49:56 -0400

    19-10-23

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+  {id: 1, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 2, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 3, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 4, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 5, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 6, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 7, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 8, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 9, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
+  {id: 10, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} 
+] ;
+
 app.get('/', (req, res) => {
   res.render('index', { message: 'Olá mundo' });
 });
+
+
 
 app.get('/pagina', (req, res) => {
   res.render('pagina', { message: 'produto' });
@@ -16,3 +31,4 @@ app.get('/pagina', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
