80a914ae973fb595653cb088f3b1be789a877c7f
Victor <victor.carvalhok23@gmail.com>
2023-10-26 16:16:48 -0400

    update imagens 26-10-23

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const produtos = [
-  {id: 1, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 2, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 3, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 4, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 5, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 6, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 7, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 8, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 9, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} ,
-  {id: 10, nome: "name", preco: "12", descricao: "sss", img: "nome.jpg"} 
+  {id: 1, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 2, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 3, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 4, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 5, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 6, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 7, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 8, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 9, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 10, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} 
 ] ;
 
 app.get('/', (req, res) => {
