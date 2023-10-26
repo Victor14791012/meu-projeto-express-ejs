465c368963cf281395939577face30edf7234859
Victor <victor.carvalhok23@gmail.com>
2023-10-26 16:50:46 -0400

    a

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.use(express.static('public'));
 
 const produtos = [
   {id: 1, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
-  {id: 2, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
-  {id: 3, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
-  {id: 4, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
-  {id: 5, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
-  {id: 6, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
-  {id: 7, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
+  {id: 2, nome: "Game - Red Dead Redemption 2 - PS4", preco: "89.99", descricao: "Jogo Brabo", img: "imagens/img-2.jpg"} ,
+  {id: 3, nome: "Game final fantasy xvi - ps5", preco: "307.99", descricao: "Jogo Bão Demais", img: "imagens/img-3.jpg"} ,
+  {id: 4, nome: "Vídeo Game Stick Lite 4K Com 15.000 Mil Jogos Clássicos + 2 Controles Sem Fio", preco: "179.90", descricao: "Console Brabo", img: "imagens/img-4.jpg"} ,
+  {id: 5, nome: "console xbox series s 500gb ssd", preco: "2.067.30", descricao: "Console Bão mas pior que Playstatrion", img: "imagens/img-5.jpg"} ,
+  {id: 6, nome: "Smart TV LED 32 HD TCL 32S615 com Design Sem Bordas Bluetooth Google Assistant e Android TV", preco: "1.199.00", descricao: "Note Brabo", img: "imagens/img-5.jpg"} ,
+  {id: 7, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Tv boa mas tem melhores", img: "imagens/img-6.jpg"} ,
   {id: 8, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
   {id: 9, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} ,
   {id: 10, nome: "Notebook Samsung NP550XDA-KV3BR Intel Core i3-1115G4 4GB 256GB SSD Tela 15,6 Windows 11 - Cinza Chumbo", preco: "2.499.90", descricao: "Note Brabo", img: "imagens/img-1.jpg"} 
